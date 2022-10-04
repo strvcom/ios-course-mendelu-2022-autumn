fb013df45053127f3dd4b6e48badc8a3fc21a408
Tina Stremenova <martina.sremenova@strv.com>
2022-10-04 18:12:15 +0200

    feat: adding node object to level 1

---
+++ SpriteKit/MendeluSpriteKit/Game/SceneObjects/Fountain.swift
@@ -0,0 +1,56 @@
+//
+//  Fountain.swift
+//  MendeluSpriteKit
+//
+//  Created by Martina Stremenova on 04.10.2022.
+//
+
+import SpriteKit
+
+final class Fountain: SKSpriteNode {
+    // MARK: Properties
+    private let waterFlowFrames = SKTextureAtlas(named: Assets.Atlas.fountainFlowing).textures
+
+    private(set) var animations = [String : SKAction]()
+}
+
+// MARK: SceneObject
+extension Fountain: SceneObject {
+    func setup(scene: LevelScene) {
+        setupAction()
+
+        setupFountain()
+    }
+
+    func update(_ currentTime: TimeInterval) {
+        playAnimation(key: Animations.flowing.rawValue)
+    }
+}
+
+// MARK: AnimatedObject
+extension Fountain: AnimatedObject {}
+
+private extension Fountain {
+    enum Animations: String {
+        case flowing
+    }
+}
+
+private extension Fountain {
+    func setupFountain() {
+        zPosition = Layer.shootingPumpkin // TODO: Modify for pumpkin
+    }
+
+    func setupAction() {
+        let timePerFrame: TimeInterval = 0.12
+
+        animations[Animations.flowing.rawValue] = SKAction.repeatForever(
+            SKAction.animate(
+                with: waterFlowFrames,
+                timePerFrame: timePerFrame,
+                resize: false,
+                restore: true
+            )
+        )
+    }
+}
