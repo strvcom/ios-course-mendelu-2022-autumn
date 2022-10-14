0fca66c0f9727506fe2553f75241d30257063352
Tina Stremenova <martina.sremenova@strv.com>
2022-10-14 15:13:10 +0200

    feat: updating level finished scenes layout

---
+++ SpriteKit/MendeluSpriteKit/Game/LevelFinishedScene.swift
@@ -0,0 +1,39 @@
+//
+//  LevelFinishedScene.swift
+//  MendeluSpriteKit
+//
+//  Created by Martina Stremenova on 12.10.2022.
+//
+
+import SpriteKit
+
+final class LevelFinishedScene: SKScene {
+    // MARK: Properties
+    private var playAgainButton: TouchNode?
+
+    var newGameButtonTapped: (() -> Void)?
+}
+
+// MARK: Public API
+extension LevelFinishedScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+
+        playAgainButton = children.first(where: { $0 is TouchNode }) as? TouchNode
+        playAgainButton?.tapAction = { [weak self] in
+            self?.newGameButtonTapped?()
+        }
+    }
+
+    func setBackgroundImage(_ image: UIImage) {
+        let background = SKSpriteNode(texture: SKTexture(image: image))
+        let aspectRatio = image.size.height / image.size.width
+        let backgroundSize = CGSize(width: size.width, height: size.width * aspectRatio)
+
+        background.size = backgroundSize
+        background.zPosition = -1
+        background.alpha = 0.3
+
+        addChild(background)
+    }
+}
